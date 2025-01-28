bd89122f2a09e1f1308971d72411928967384267
fo <alfonsomartingarcia@gmail.com>
2025-01-28 13:45:28 +0000

    adding definitions

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -9,7 +9,12 @@ return {
 		-- Document existing key chains
 		require("which-key").add({
 
+			{ "<leader>c", group = "[C]ode stuff" },
+			{ "<leader>d", group = "[D]iagnostics" },
 			{ "<leader>f", group = "[F]ind / Search options" },
+			{ "<leader>g", group = "[G]it stuff" },
+			{ "<leader>h", group = "[H] more Git stuff" },
+			{ "<leader>k", group = "[k]ill buffers" },
 			{ "<leader>l", group = "[L] Git / Sessions" },
 			{ "<leader>n", group = "[N]ew note or file" },
 			{ "<leader>s", group = "[S]ubstitute current work locally / globaly" },
