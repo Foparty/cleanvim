271767d490b7906a26c170106ae705ed85d845ef
fo <alfonsomartingarcia@gmail.com>
2025-01-29 13:34:38 +0000

    scheme changes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,40 +1,44 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = false,
-      })
-      -- vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        transparent = false,
-      })
-    end,
-  },
-  {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    config = function()
-      require("github-theme").setup({
-        options = {
-          transparent = true,
-        },
-      })
-    end,
-  },
+	{
+		-- https://github.com/catppuccin/nvim
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = false,
+			})
+			-- vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		-- https://github.com/folke/tokyonight.nvim
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = false,
+			})
+		end,
+	},
+	{
+		-- https://github.com/zenbones-theme/zenbones.nvim
+		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		-- https://github.com/projekt0n/github-nvim-theme
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		config = function()
+			require("github-theme").setup({
+				options = {
+					transparent = true,
+				},
+			})
+		end,
+	},
 }
