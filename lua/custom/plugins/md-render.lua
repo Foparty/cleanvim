bc613bc980e3d9dd116d8f32b2ab311df9a76487
fo <alfonsomartingarcia@gmail.com>
2025-02-03 14:37:39 +0000

    markdown renders off... can't get anything that i like

--- lua/custom/plugins/md-render.lua
+++ lua/custom/plugins/md-render.lua
@@ -1,19 +1,29 @@
 return {
-	{
-		"OXY2DEV/markview.nvim",
-		enabled = true,
-		lazy = false,
-		ft = "markdown",
-		config = function()
-			require("markview").setup({
-				-- hybrid_modes = { "n", "i" },
-				preview = {
-					hybrid_modes = { "n", "i" },
-				},
-			})
-			require("markview.extras.checkboxes").setup()
-			require("markview.extras.editor").setup({})
-			require("markview.extras.headings").setup()
-		end,
-	},
+  {
+    "OXY2DEV/markview.nvim",
+    enabled = false,
+    lazy = false,
+    ft = "markdown",
+    config = function()
+      require("markview").setup({
+        -- hybrid_modes = { "n", "i" },
+        preview = {
+          hybrid_modes = { "n", "i" },
+        },
+      })
+      require("markview.extras.checkboxes").setup()
+      require("markview.extras.editor").setup({})
+      require("markview.extras.headings").setup()
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
