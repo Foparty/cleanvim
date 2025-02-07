1ef433249f7cd84031a5b174a81ec7772e6d02eb
fo <alfonsomartingarcia@gmail.com>
2025-02-07 10:25:52 +0000

    trying to short the keys

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -1,24 +1,24 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup({
-			preset = "modern",
-		})
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require("which-key").setup({
+      preset = "helix",
+    })
 
-		-- Document existing key chains
-		require("which-key").add({
+    -- Document existing key chains
+    require("which-key").add({
 
-			{ "<leader>c", group = "[C]ode stuff" },
-			{ "<leader>d", group = "[D]iagnostics" },
-			{ "<leader>f", group = "[F]ind / Search options" },
-			{ "<leader>g", group = "[G]it stuff" },
-			{ "<leader>h", group = "[H] more Git stuff" },
-			{ "<leader>k", group = "[k]ill buffers" },
-			{ "<leader>l", group = "[L] Git / Sessions" },
-			{ "<leader>n", group = "[N]ew note or file" },
-			{ "<leader>s", group = "[S]ubstitute current work locally / globaly" },
-			-- { "<leader>t", group = "[T]oggle Pencil" },
-		})
-	end,
+      { "<leader>c", group = "[C]ode stuff" },
+      { "<leader>d", group = "[D]iagnostics" },
+      { "<leader>f", group = "[F]ind / Search options" },
+      { "<leader>g", group = "[G]it stuff" },
+      { "<leader>h", group = "[H] more Git stuff" },
+      { "<leader>k", group = "[H] more Git stuff" },
+      { "<leader>l", group = "[L] Git / Sessions" },
+      { "<leader>n", group = "[N]ew note or file" },
+      { "<leader>s", group = "[S]ubstitute current work locally / globaly" },
+      -- { "<leader>t", group = "[T]oggle Pencil" },
+    })
+  end,
 }
