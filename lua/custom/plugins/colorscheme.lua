a011ba6500111f81638d7d63c76a5160f4e5798c
fo <103907142+Foparty@users.noreply.github.com>
2025-03-23 13:43:59 +0000

    final changes for copilot and theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,56 +1,59 @@
 return {
-	{
-		-- https://github.com/catppuccin/nvim
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = true,
-			})
-			-- vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	{
-		-- https://github.com/folke/tokyonight.nvim
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				transparent = false,
-			})
-		end,
-	},
-	{
-		-- https://github.com/zenbones-theme/zenbones.nvim
-		"zenbones-theme/zenbones.nvim",
-		dependencies = "rktjmp/lush.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		-- https://github.com/projekt0n/github-nvim-theme
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = true,
-				},
-			})
-		end,
-	},
-	{
-		"olivercederborg/poimandres.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("poimandres").setup({
-				-- leave this setup function empty for default config
-				-- or refer to the configuration section
-				-- for configuration options
-			})
-		end,
-	},
+  {
+    -- https://github.com/catppuccin/nvim
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+      -- vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    -- https://github.com/folke/tokyonight.nvim
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = false,
+      })
+    end,
+  },
+  {
+    -- https://github.com/zenbones-theme/zenbones.nvim
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    -- https://github.com/projekt0n/github-nvim-theme
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+  },
+  {
+    "datsfilipe/vesper.nvim",
+  },
 }
