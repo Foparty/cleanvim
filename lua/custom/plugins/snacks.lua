ad55897efccb0615004d38ade6148e3328543956
fo <103907142+Foparty@users.noreply.github.com>
2025-03-23 21:37:34 +0000

    snacks explorer, pickers

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -2,32 +2,148 @@ return {
   -- lazy.nvim
   {
     "folke/snacks.nvim",
-    enabled = false,
+    enabled = true,
     ---@type snacks.Config
     opts = {
       explorer = {
         replace_netrw = true,
-        -- your explorer configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
       },
+      notify = { enabled = true },
+      notifier = { enabled = true },
+      terminal = { enabled = true },
       picker = {
+        layout = {
+          -- fullscreen = true,
+          cycle = false,
+          layout = {
+            width = 0.95,
+            height = 0.95,
+          },
+        },
+        matcher = {
+          frecency = true,
+        },
         sources = {
           explorer = {
+            -- hidden = true,
+            -- ignored = true,
+            -- exclude = { ".git" },
             auto_close = true,
-            -- your explorer picker configuration comes here
-            -- or leave it empty to use the default settings
+            layout = {
+              layout = {
+                width = 0.2,
+                height = 1,
+              },
+              fullscreen = false,
+            },
           },
         },
       },
     },
     keys = {
+
       {
-        "<leader>e",
+        "<C-e>",
         function()
-          Snacks.picker.explorer()
+          Snacks.explorer()
         end,
-        desc = "Explorer",
+        desc = "Explorer Toggle",
+      },
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.smart({
+            finders = { "files" },
+            filter = { cwd = true },
+          })
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>/",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "Grep",
+      },
+      {
+        "<leader>.",
+        function()
+          Snacks.terminal()
+        end,
+      },
+      {
+        "<leader>fc",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+        end,
+        desc = "Find Config File",
+      },
+      {
+        "<leader>fn",
+        function()
+          Snacks.picker.files({ cwd = "~/notes/" })
+        end,
+        desc = "Find Notes",
+      },
+      {
+        "<leader>fm",
+        function()
+          Snacks.picker.grep({ cwd = "~/notes/" })
+        end,
+        desc = "Grep Notes",
+      },
+      --lsp
+
+      {
+        "gd",
+        function()
+          Snacks.picker.lsp_definitions()
+        end,
+        desc = "Goto Definition",
+      },
+      {
+        "gD",
+        function()
+          Snacks.picker.lsp_declarations()
+        end,
+        desc = "Goto Declaration",
+      },
+      {
+        "gr",
+        function()
+          Snacks.picker.lsp_references()
+        end,
+        nowait = true,
+        desc = "References",
+      },
+      {
+        "gI",
+        function()
+          Snacks.picker.lsp_implementations()
+        end,
+        desc = "Goto Implementation",
+      },
+      {
+        "gy",
+        function()
+          Snacks.picker.lsp_type_definitions()
+        end,
+        desc = "Goto T[y]pe Definition",
+      },
+      {
+        "<leader>ss",
+        function()
+          Snacks.picker.lsp_symbols()
+        end,
+        desc = "LSP Symbols",
+      },
+      {
+        "<leader>sS",
+        function()
+          Snacks.picker.lsp_workspace_symbols()
+        end,
+        desc = "LSP Workspace Symbols",
       },
     },
   },
