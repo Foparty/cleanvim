b187891ef215fe4d7d5b45e47dd64d2477a781e8
fo <103907142+Foparty@users.noreply.github.com>
2025-03-23 21:37:12 +0000

    telescope config out

--- lua/custom/plugins/sessions.lua
+++ lua/custom/plugins/sessions.lua
@@ -1,23 +1,14 @@
 return {
-  {
-    "rmagatti/auto-session",
-    lazy = false,
-    config = function()
-      require("auto-session").setup({
-        log_level = "error",
-        suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        session_lens = {
-          load_on_startup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-        vim.keymap.set(
-          "n",
-          "<leader>ls",
-          require("auto-session.session-lens").search_session,
-          { desc = "[L]ist [S]essions" }
-        ),
-      })
-    end,
-  },
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			-- log_level = 'debug',
+		},
+	},
 }
