d37ad4f645d197273334d791b1138bec42def6f1
fo <103907142+Foparty@users.noreply.github.com>
2025-03-30 20:55:57 +0100

    testing mini.diff

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,12 +1,13 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    enabled = true,
-    config = function()
-      require("mini.surround").setup({})
-      require("mini.ai").setup({ n_lines = 500 })
-      require("mini.pairs").setup()
-      require("mini.splitjoin").setup()
-    end,
-  },
+	{
+		"echasnovski/mini.nvim",
+		enabled = true,
+		config = function()
+			require("mini.surround").setup({})
+			require("mini.ai").setup({ n_lines = 500 })
+			require("mini.pairs").setup()
+			require("mini.splitjoin").setup()
+			-- require("mini.diff").setup()
+		end,
+	},
 }
