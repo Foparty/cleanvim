5f28cad9049301e927a859c5ed57b01ae2afff8d
fo <103907142+Foparty@users.noreply.github.com>
2025-04-03 07:29:42 +0100

    adding stuff to test if it works

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -33,6 +33,31 @@ return {
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
+      fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        max_typos = function(keyword)
+          return math.floor(#keyword / 4)
+        end,
+        use_frecency = true,
+        use_proximity = true,
+        use_unsafe_no_lock = false,
+        sorts = {
+          "score",
+          "sort_text",
+        },
+
+        prebuilt_binaries = {
+          download = true,
+
+          ignore_version_mismatch = false,
+
+          force_version = nil,
+
+          force_system_triple = nil,
+
+          extra_curl_args = {},
+        },
+      },
     },
     opts_extend = { "sources.default" },
   },
